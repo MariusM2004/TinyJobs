737bb2b5b1185000b52efccf1bec4046d800e6a8
Marius <mermeze.abcd@gmail.com>
2021-04-10 11:49:43 +0300

    login page changes

---
+++ angular/src/app/account/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    const splash = document.querySelector('.splash');
+
+    document.addEventListener('DOMContentLoaded', (e) => {
+      setTimeout(() => {
+        splash?.classList.add('display-none');
+      }, 2000);
+    })
+  }
+
+}
